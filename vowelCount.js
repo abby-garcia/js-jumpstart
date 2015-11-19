ee4487424cb50204179985b5a135d3c40e1bdebb
abby-garcia <acg361@gmail.com>
2015-11-19 15:21:35 -0500

    writing better instructions

--- vowelCount.js
+++ vowelCount.js
@@ -15,48 +15,33 @@
 //The way they like doing it
 
 
-function vowelCounts(str){
-	var lowerStr = str.toLowerCase();
-
-	for(var i = 0; i < lowerStr.length; i++){
-		lowerStr[i];
-
+function vowelCount(word){
+	var lowerWord = word.toLowerCase();
+	var vowels = ['a', 'e', 'i', 'o' 'u'];
+	var counter = 0;
+	for(var i =0; i < lowerWord.length; i++){
+		if(vowels.indexOf(lowerWord[i]) > -1 ){
+			counter ++;
+		}
 	}
+
+
+
+	
 
 
 
 }
 
 
-vowelCounts("Good job!");
 
-
-
-
-
-
-
-
-// you need to find the vowels in a string
-//create a loop to be able to find certain letters within a string
-// Step 1 - create a variable that makes all letters lower case
-//Step 2 - create a for loop.
-//Step 3 - console log line 22 to make sure that it is sending you the right string. 
-
-
-//SHORT WAY
-
-
-// function countVowels(subject) {
-//     return subject.match(/[aeiou]/gi).length;
-// }
-
-
-// countVowels("Elder Price.");
-
-
-// so we need to find a way to count ONLY the vowels. That's not too bad. 
-//How do we get the w
-//so you need to split the word, and if any of the words is equal to
-// a e i o u, you count it?
-//how do you count?
+//NOTES
+//1.  one of the first things you do in a problem like this is makeing sure you lowercase everything.
+//2. identify the vowels
+//3. create a counter and it's start number, so it can increase every time it finds a vowle.
+//4. anytime you are looking for a letter within anything, you need to have a for loop. 
+//5. create you typical for loop
+//6. in line 23, you are saying, IF the index of a certain letter is greter than -1, it's a vowel.
+//7. Why is that? when -1 appears when doing an "indexOf", it means that the numer DOES NOT EXISIT. 
+//8. The number EXISIT if it is ZERO and UP. 
+//9. Line 24, tells you to increase the counter
