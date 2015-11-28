a2cc2813fe5659f5a9fcd3083d3541bcdd84b0b9
abby-garcia <acg361@gmail.com>
2015-11-28 11:04:15 -0500

    more problems

---
+++ nestedLoops.js
@@ -0,0 +1,24 @@
+Create the function "rightTriangle", it returns a string
+represeniting a right Triangleusing " * ". 
+The number argument represents the number of ros the triangle will have. 
+
+function rightTriangle(rows){
+	var finalString = "";
+
+	for(var row = rows; row > 0; row--){
+		for(var noStars = 0; noStars < row; noStars++){
+			finalString = finalString + "*";
+
+		}
+	
+		finalString += "\n";
+	}
+	return finalString;
+}
+
+rightTriangle(5);
+
+
+//The \n character is used to find a newline character.
+// you need to make it add a certain amount of stars
+
