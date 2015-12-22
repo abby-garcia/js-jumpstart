60a3a78c1c9226b2cc660235bcd562c1229a5e3f
abby-garcia <acg361@gmail.com>
2015-12-22 13:27:11 -0500

    overlap

--- overlapping.js
+++ overlapping.js
@@ -7,10 +7,25 @@
 // also be a positive integer, and return the string 
 // true if both sets of ranges overlap by at least x numbers. 
 // For example: if arr is [4, 10, 2, 6, 3] then your
-//  program should return the string true. 
-//  The first range of numbers are 4, 5, 6, 7, 8, 9, 10 and 
-//  the second range of numbers are 2, 3, 4, 5, 6. 
-//  The last element in the array is 3, and there are
-//   3 numbers that overlap between both ranges: 4, 5, and 6. 
-//   If both ranges do not overlap by at least x numbers, 
-//   then your program should return the string false. 
+// program should return the string true. 
+// The first range of numbers are 4, 5, 6, 7, 8, 9, 10 and 
+// the second range of numbers are 2, 3, 4, 5, 6. 
+// The last element in the array is 3, and there are
+// 3 numbers that overlap between both ranges: 4, 5, and 6. 
+// If both ranges do not overlap by at least x numbers, 
+// then your program should return the string false. 
+
+
+OverlappingRanges(arr){
+
+
+
+}
+OverlappingRanges([4, 10, 2, 6, 3]);
+
+
+
+
+
+
+
